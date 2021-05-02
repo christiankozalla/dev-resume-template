5161b45501b75f04d48c77d4bb071f4822d56cdb
christiankozalla <christian.kozalla@gmail.com>
2021-05-02 19:49:07 +0200

    Updates vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-const { resolve } = require("path");
-
 module.exports = {
   base: "/dev-resume-template/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        de: resolve(__dirname, "de/index.html"),
-      },
-    },
-  },
 };
